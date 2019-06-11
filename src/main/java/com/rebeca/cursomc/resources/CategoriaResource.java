d060ae22635c982ef99b5c46ce71a374060893cd
Rebeca Divina <rebecadivinapc@gmail.com>
2019-06-11 15:07:21 -0300

    Testando a categoria

--- src/main/java/com/rebeca/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/rebeca/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,28 @@
 package com.rebeca.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.rebeca.cursormc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categoria")
 public class CategoriaResource {
 	@RequestMapping(method=RequestMethod.GET)
-	private String listar() {
-		return "REST esta dando bom";
+	private List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1,"Informatica");
+		Categoria cat2 = new Categoria(2,"Escritorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 
 	}
 }
